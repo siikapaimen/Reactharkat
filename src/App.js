f107066903db00c2c4a8276b6e9ff3669127aefc
Kinnunen Suzan <a1503022@myy.haaga-helia.fi>
2017-03-16 17:05:50 +0200

    Reactin perustehtavat valmiiit verta ja kyyneliä vuodatettu'd

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ class App extends Component {
     
     constructor(props){
         super(props);
-        this.state={name:''}
+        this.state={name:'', value:'', count:0}
+        this.handleChange = this.handleChange.bind(this);
+        //button?
+       this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+    
+    //inputinyritystähandlechangeeventilläää
+      handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+    
+    //submittiaaa?
+     handleSubmit(event) {
+        //alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+        this.setState({count: this.state.count +1});
+  }
     
     componentDidMount(){
         this.setState({
-        name:'Suzan'
+        name:'Suzan'//,
+        //count:0
         });
     }
     
@@ -20,7 +37,15 @@ class App extends Component {
     return (
       <div className="App">
         
-            <h1>Hello, {this.state.name}</h1>    
+            <h1>Hello, {this.state.name}</h1> 
+        
+            <h1>Terve, {this.state.value}</h1>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+        
+    <form onSubmit={this.handleSubmit}>   
+        <p>Painiketta klikattu {this.state.count} kertaa</p>
+        <button onClick={this.handleSubmit}>klikken</button>
+    </form>
     
       </div>
     );
